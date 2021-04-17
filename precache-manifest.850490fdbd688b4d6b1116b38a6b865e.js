142f7bdfca7d1cf05ec6ba086e7ef9449e22c858
Om Kathe <omkathe26@gmail.com>
2021-04-17 22:45:17 +0530

    Updates

---
+++ precache-manifest.850490fdbd688b4d6b1116b38a6b865e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0c3c1b44633558cca178875f7295ab",
+    "url": "/clockApp/index.html"
+  },
+  {
+    "revision": "a799ed64f82e8634524d",
+    "url": "/clockApp/static/css/main.d69be4b0.chunk.css"
+  },
+  {
+    "revision": "624690653c60032e1913",
+    "url": "/clockApp/static/js/2.babcf76e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clockApp/static/js/2.babcf76e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a799ed64f82e8634524d",
+    "url": "/clockApp/static/js/main.8098edf2.chunk.js"
+  },
+  {
+    "revision": "f8263bdc3971d5c5aae9",
+    "url": "/clockApp/static/js/runtime-main.6b5ad66b.js"
+  }
+]);
